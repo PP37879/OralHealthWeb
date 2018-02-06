ebd619f2fa872086fb6ccb7a76a69f2b2c43677f
bbookk <book.maytawee@gmail.com>
2018-02-06 16:29:38 +0700

    add dialog to show complete in import

--- OralHealth/angular-src/src/download.js
+++ OralHealth/angular-src/src/download.js
@@ -6,8 +6,15 @@ function chooseFile() {
     var input = $(document.createElement("input"));
     input.attr("type", "file");
     input.trigger("click"); // opening dialog
-    input.change(function(){
+    input.change(function () {
         alert("file Saved");
         window.location.href = "user.html";
+    });
+}
+function showModal() {
+    $(document.getElementById("myModal")).ready(function() { 
+        setTimeout(function() { 
+            $('#myModal').modal('hide');
+     }, 1000); 
     });
 }
